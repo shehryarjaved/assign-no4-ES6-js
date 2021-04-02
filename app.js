e31e6579dd00b5a854b0d45815b337261c035b4e
ShehryarJaved <shehryarJaved007@gmail.com>
2021-04-03 00:57:53 +0500

    Assignmnet 4 Es6 done

---
+++ app.js
@@ -0,0 +1,32 @@
+// Example of higher order function
+
+// const name = [];
+//    var persons = [
+//       { name: 'Frodobaggins'},
+//       { name: 'aragorn'},
+//       { name: 'gandalf'},
+//       { name: 'pippin'},
+//       { name: 'nazgul'},
+//    ];
+//    for(let i = 0; i < persons.length; i++) {
+//       if(persons[i].name.length > 6) {
+//          name.push(persons[i]);
+//       }
+//    }
+// document.write(JSON.stringify(name));
+
+
+// Example Closures in JavaScript 
+
+// var p = 10;
+// function a() {
+//    var p = 15;
+//    b(function() {
+//       alert(p);
+//    });
+// }
+// function b(f){
+//    var p = 30;
+//    f();
+// }
+// a();
